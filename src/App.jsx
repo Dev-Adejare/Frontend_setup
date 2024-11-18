0104b5fec7e162b1da5af95c40a82ae0378d4344
root <root@ADEJARE-02>
2024-11-18 13:42:10 +0100

    Layout_update

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./config/connection"
+import Layout from './components/Layout'
+function App() {
+  
+
+  return (
+    <Layout>
+      <h1>Todo Dapp</h1>
+    </Layout>
+  )
+}
+
+export default App
---
+++ src/components/Layout.jsx
@@ -0,0 +1,26 @@
+import { Text } from "@radix-ui/themes"
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <main className="w-full min-h-screen flex flex-col justify-between bg-stone-950">
+        {/* header */}
+        <div as="header"className="w-full h-20 flex justify-between items-center px-4 bg-amber-500">
+            <Text as= "h3" className= "text-xl font-bold"> Todo Dapp</Text>
+            <appkit-button />
+            {/* <Button className= "bg-stone-900 text-stone-200 px-6 py-2 rounded-md">Connect</Button> */}
+        </div>
+        <section className="flex-1">
+            {children}
+        </section>
+        {/* Footer */}
+        <footer className="w-full h-20 flex justify-center items-center">
+            <Text as="p" className= "text-stone-200">Todo Dapp &copy; 2024. All Right Reserved.</Text>
+        </footer>
+
+      </main>
+    </div>
+  )
+}
+
+export default Layout
